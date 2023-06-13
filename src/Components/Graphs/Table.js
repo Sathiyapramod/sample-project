639a6e4f1cbd66f37a1f20b32dfe7a9347acf175
Sathiyapramod <sathiyapramod22@gmail.com>
2023-06-13 18:53:50 +0530

    *

--- src/Components/Graphs/Table.js
+++ src/Components/Graphs/Table.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Switch from "@mui/material/Switch";
-import { Chip, Table, TablePagination } from "@mui/material";
+import { Chip, Table, TableContainer, TablePagination } from "@mui/material";
 import { TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import Checkbox from "@mui/material/Checkbox";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
@@ -74,92 +74,95 @@ function MyTable() {
   };
   return (
     <>
-      <Table style={{ height: 400, width: "100%", backgroundColor: "#FFFFFF" }}>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <Checkbox />
-            </TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell>Campaign Name</TableCell>
-            <TableCell sx={{ width: 120 }}>Info</TableCell>
-            <TableCell sx={{ width: 120 }}>Automation</TableCell>
-            <TableCell>Dayparting</TableCell>
-            <TableCell>Budget</TableCell>
-            <TableCell>Bidding Strategy</TableCell>
-            <TableCell>Spend</TableCell>
-            <TableCell>Revenue</TableCell>
-            <TableCell>Current Bid</TableCell>
-            <TableCell>Conv</TableCell>
-            <TableCell>Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((element, index) => {
-            return (
-              <TableRow key={index}>
-                <TableCell>
-                  <Checkbox />
-                </TableCell>
-                <TableCell>{element.Status}</TableCell>
-                <TableCell>{element.CampaignName}</TableCell>
-                <TableCell>
-                  {element.Info.map((element, index) => {
-                    return (
-                      <Chip
-                        key={index}
-                        label={element}
-                        size="small"
-                        variant="outlined"
-                      />
-                    );
-                  })}
-                </TableCell>
-                <TableCell>
-                  {element.Automation ? (
-                    <Switch defaultChecked disabled />
-                  ) : (
-                    <Switch disabled />
-                  )}
-                </TableCell>
-                <TableCell>
-                  {element.Dayparting ? (
-                    <Switch defaultChecked />
-                  ) : (
-                    <Switch disabled />
-                  )}
-                </TableCell>
-                <TableCell>${element.Budget}</TableCell>
-                <TableCell>
-                  <a rel="noreferrer" href="1">
-                    {element.BiddingStrategy}
-                  </a>
-                </TableCell>
-                <TableCell>${element.Spend}</TableCell>
-                <TableCell>${element.Revenue}</TableCell>
-                <TableCell>${element.CurrentBid}</TableCell>
-                <TableCell>{element.Conv}%</TableCell>
-                <TableCell>
-                  <MoreVertIcon />
-                </TableCell>
-              </TableRow>
-            );
-          })}
-          <TableRow align="center">
-            <TableCell colSpan={13}>
-              {" "}
-              <TablePagination
-                rowsPerPageOptions={[3, 4, 5]}
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-              />
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+      <TableContainer>
+        <Table
+          style={{ height: 400, width: "100%", backgroundColor: "#FFFFFF" }}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Checkbox />
+              </TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Campaign Name</TableCell>
+              <TableCell sx={{ width: 120 }}>Info</TableCell>
+              <TableCell sx={{ width: 120 }}>Automation</TableCell>
+              <TableCell>Dayparting</TableCell>
+              <TableCell>Budget</TableCell>
+              <TableCell>Bidding Strategy</TableCell>
+              <TableCell>Spend</TableCell>
+              <TableCell>Revenue</TableCell>
+              <TableCell>Current Bid</TableCell>
+              <TableCell>Conv</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((element, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell>
+                    <Checkbox />
+                  </TableCell>
+                  <TableCell>{element.Status}</TableCell>
+                  <TableCell>{element.CampaignName}</TableCell>
+                  <TableCell>
+                    {element.Info.map((element, index) => {
+                      return (
+                        <Chip
+                          key={index}
+                          label={element}
+                          size="small"
+                          variant="outlined"
+                        />
+                      );
+                    })}
+                  </TableCell>
+                  <TableCell>
+                    {element.Automation ? (
+                      <Switch defaultChecked disabled />
+                    ) : (
+                      <Switch disabled />
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {element.Dayparting ? (
+                      <Switch defaultChecked />
+                    ) : (
+                      <Switch disabled />
+                    )}
+                  </TableCell>
+                  <TableCell>${element.Budget}</TableCell>
+                  <TableCell>
+                    <a rel="noreferrer" href="1">
+                      {element.BiddingStrategy}
+                    </a>
+                  </TableCell>
+                  <TableCell>${element.Spend}</TableCell>
+                  <TableCell>${element.Revenue}</TableCell>
+                  <TableCell>${element.CurrentBid}</TableCell>
+                  <TableCell>{element.Conv}%</TableCell>
+                  <TableCell>
+                    <MoreVertIcon />
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+            <TableRow>
+              <TableCell colSpan={13}>
+                <TablePagination
+                  rowsPerPageOptions={[3, 4, 5]}
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 }
